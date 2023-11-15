18687c6241cddf3a487b866af94a8f66397c6b61
Tornike Despotashvili <despo@Tornikes-MacBook-Air.local>
2023-11-15 15:13:35 +0400

    added timestamp

--- clinet/src/pages/Evernote.js
+++ clinet/src/pages/Evernote.js
@@ -2,22 +2,21 @@ import React from 'react'
 import "./styles/evernote.css"
 import NotesDashboard from '../components/notesDashbouard/NotesDashboard'
 import ScratchPad from '../components/scratchPad/ScratchPad'
+import CurrentDate from '../components/date/CurrentDate'
 
 export default function Evernote() {
   return (
     <div>
-        
-
         <div className='main_container'>
+        <CurrentDate />
           
             <div className='main_container_bg' />
               
             <div className='top_widgets'>
               <NotesDashboard />
-
+              
               <ScratchPad />
             </div> 
-            
             
         </div>
     </div>
