ac024469447699fe4a54c137efb560aa2fb50699
Tornike Despotashvili <despo@Tornikes-MacBook-Air.local>
2023-11-18 01:02:01 +0400

    added routes and mysql logic

--- api/index.js
+++ api/index.js
@@ -3,6 +3,8 @@ import cors from "cors"
 import cookieParser from "cookie-parser"
 import multer from "multer"
 import path from "path"
+import { userLogin } from "./routes/users.js"
+import { createNote, getNotes, getSingleNote, deleteSingleNote} from "./routes/notes.js"
 
 const app = express()
 app.use(cors({origin: 'http://localhost:3000', credentials: true})); 
@@ -16,5 +18,11 @@ app.get('/', (req, res)=>{
     res.send('hello world')
 })
 
+
+app.post('/googleauth', userLogin)
+app.post('/createnote', createNote)
+app.get('/getnotes', getNotes)
+app.get('/notes/:id', getSingleNote)
+app.delete('/notes/deletenote/:id', deleteSingleNote)
 
 app.listen(port)
